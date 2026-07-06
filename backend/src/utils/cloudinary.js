1686894fa6553a2e46549b73abdb5c8bae5693db
shivanshu_tod <shivanshunigam8@gmail.com>
2026-07-06 15:41:47 +0530

    asdadasd

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,5 +1,5 @@
 const cloudinary=require('cloudinary').v2;
 cloudinary.config({cloud_name:process.env.CLOUDINARY_CLOUD_NAME,api_key:process.env.CLOUDINARY_API_KEY,api_secret:process.env.CLOUDINARY_API_SECRET});
-const uploadFile=(path,folder='aligno')=>cloudinary.uploader.upload(path,{folder,resource_type:'auto'});
+const uploadFile=(path,folder='aligno',options={})=>cloudinary.uploader.upload(path,{folder,resource_type:'auto',quality:'auto:best',fetch_format:'auto',...options});
 const deleteFile=(publicId)=> publicId ? cloudinary.uploader.destroy(publicId) : Promise.resolve();
 module.exports={cloudinary,uploadFile,deleteFile};
